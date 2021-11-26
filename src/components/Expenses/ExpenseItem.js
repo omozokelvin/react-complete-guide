245cff5165483d08daa8def20453ee9d57186d61
Kelvin <kelvinjune2@chaka.ng>
2021-11-26 15:56:47 +0100

    added conditional rendering and rendering loops

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,24 +5,17 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated');
-
-  const clickHandler = () => {
-    setTitle('Updated!!!');
-    console.log(title);
-  };
-
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
 
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change title</button>
-    </Card>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
